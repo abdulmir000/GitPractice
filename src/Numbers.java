8c499998f4628d92043bc5878ed26769ed492287
applebay <mira861126@gmail.com>
2023-11-08 18:22:12 +0000

    Numbers task updated

---
+++ src/Numbers.java
@@ -0,0 +1,8 @@
+public class Numbers {
+
+    // Write method which can identifies given number is odd or even
+
+    // Ex :
+    //     identify (5) >> Odd number;
+    //     identify (6) >> Even number;
+}
